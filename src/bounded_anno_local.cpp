c7323c4a15c286fb7f7c8bfe4e2cdbb4df982f12
Jingwen Ren <jingwenr@usc.edu>
2022-01-04 16:04:52 -0800

    Fix some bug in data type

--- src/bounded_anno_local.cpp
+++ src/bounded_anno_local.cpp
@@ -1,14 +1,3 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-using namespace std;
-
-
 // function to compute the distance of given pair of characters
 static double distance(char a, char b) {
 
@@ -39,84 +28,82 @@ static int compare(vector<double> &update, int l) {
     return k;
 }
 
-
 // function to compute the variant N-W alignment (linear space, only computes opt distance and traces starting position)
 // (leading gaps cost 0)
 static void
-global(const string &motif, char *vntr, int n, vector<double> &dist, vector<uint8_t> &start) { 
+global(const string &motif, char *vntr, vector<vector<double>> &dists, vector<vector<int>> &starts, int m, int N) { 
 
-    int i, j, k;
-    uint8_t oldStart, tempStart; // i as row (string A) index, j as column (string B) index, k as distance index for "compare" function
-    int m = motif.size();
+    uint8_t i, k;
+    int j, oldStart, tempStart; // i as row (string A) index, j as column (string B) index, k as distance index for "compare" function
+    uint8_t M = motif.size();
     double oldDist;
     vector<double> update(3);
 
     // initialization for the first row (dist=0, start=j for first row, 0 penalty for leading gaps)
-    for (j=0; j<=n; j++) {
-        dist[j] = 0;
-        start[j] = j;
+    for (j=0; j<=N; j++) {
+        dists[m][j] = 0;
+        starts[m][j] = j;
     }
 
     // propagate to later rows (dist=i, start=0 for first column)
-    for (i=1; i<=m; i++) {
-        oldDist = dist[0];
-        oldStart = start[0];
-        dist[0] = i;
-        start[0] = 0;
+    for (i = 1; i <= M; i++) {
+        oldDist = dists[m][0];
+        oldStart = starts[m][0];
+        dists[m][0] = i;
+        starts[m][0] = 0;
 
-        for (j=1; j<=n; j++) {
+        for (j=1; j<=N; j++) {
 
             // dynamic programming
-            update[0] = dist[j-1] + distance(vntr[j-1], '-'); // use current value: dist[j-1] after update
-            update[1] = dist[j] + distance('-', motif[i-1]); // use old value: dist[j] before update
+            update[0] = dists[m][j-1] + distance(vntr[j-1], '-'); // use current value: dist[j-1] after update
+            update[1] = dists[m][j] + distance('-', motif[i-1]); // use old value: dist[j] before update
             update[2] = oldDist + distance(vntr[j-1], motif[i-1]); // use old value: dist[j-1] before update
 
             k = compare(update, 3);
 
             if (k == 0) {
-                tempStart = start[j-1];
+                tempStart = starts[m][j-1];
             } else if (k == 2) {
                 tempStart = oldStart;
             } else {
-                tempStart = start[j];
+                tempStart = starts[m][j];
             }
 
-            oldDist = dist[j]; // save old value: dist[j-1] before update
-            oldStart = start[j]; // save old value: start[j-1] before update
+            oldDist = dists[m][j]; // save old value: dist[j-1] before update
+            oldStart = starts[m][j]; // save old value: start[j-1] before update
 
             // update dist and start
-            dist[j] = update[k];
-            start[j] = tempStart;
+            dists[m][j] = update[k];
+            starts[m][j] = tempStart;
         }
     }
 }
 
-
-// function to compute the S_i distances (O(nm) time)
-int bounded_anno(vector<uint8_t> &optMotifs, vector<MOTIF> &motifs, char *vntr, int n) 
+// function to compute the S_i distances (the naive occurrence)
+void bounded_anno(vector<uint8_t> &optMotifs, vector<MOTIF> &motifs, char *vntr, int n) 
 {
     vector<uint8_t> optAnno;
-    if ((vntr != NULL) && (vntr[0] == '\0')) {
-       printf("vntr sequence is empty\n");
-    }
     optAnno.clear();
-    int i, k, m; // i as position index (of vntr), m as motif index, k as distance index for "compare" function
-    int M = motifs.size();
+    int i; // i as position index (of vntr), m as motif index, k as distance index for "compare" function
+    uint8_t M = motifs.size();
+    uint8_t k, m;
 
     vector<double> dist(n + 1);
     vector<double> update(M);
-    vector<int> gap(M);
-    vector<uint8_t> traceI(n + 1);
+    vector<bool> gap(M);
+    vector<int> traceI(n + 1);
     vector<uint8_t> traceM(n + 1);
     double ratio = 0.80;
 
     // generate dp matrix for each motif
     vector<vector<double>> dists(M);
-    vector<vector<uint8_t>> starts(M);
-    for (i = 0; i < M; ++i) {dists[i].resize(n + 1);}
-    for (i = 0; i < M; ++i) {starts[i].resize(n + 1);}
+    vector<vector<int>> starts(M);
+    for (i = 0; i < M; ++i) {dists[i].resize(n + 1, 0);}
+    for (i = 0; i < M; ++i) {starts[i].resize(n + 1, 0);}
 
-    for (m = 0; m < M; m++) global(motifs[m].seq, vntr, n, dists[m], starts[m]);
+    for (m = 0; m < M; m++) {
+        global(motifs[m].seq, vntr, dists, starts, m, n);
+    }
 
     // calculate S_i distances
     // initialization for i=0
@@ -125,8 +112,8 @@ int bounded_anno(vector<uint8_t> &optMotifs, vector<MOTIF> &motifs, char *vntr, 
     traceM[0] = 0;
 
     // propagate
-    for (i=1; i<=n; i++) {
-        for (m=0; m<M; m++) {
+    for (i = 1; i <= n; i++) {
+        for (m = 0; m < M; m++) {
             if (dists[m][i] > motifs[m].len * ratio){
                 update[m] = dist[starts[m][i]] + motifs[m].len * ratio;
                 gap[m] = 1;
@@ -162,7 +149,5 @@ int bounded_anno(vector<uint8_t> &optMotifs, vector<MOTIF> &motifs, char *vntr, 
     }
 
     assert(optMotifs.size() > 0);
-    return 0;
+    return;
 }
-
-
