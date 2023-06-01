ae4c271f3a615b8c289ee033af47e49eaafb1dad
Mark Chaisson <mchaisso@usc.edu>
2023-06-01 16:50:47 -0700

    Bump version.

--- src/io.h
+++ src/io.h
@@ -36,7 +36,7 @@ public:
 	IO () 
 	{
 		version = (char *) malloc(7);
-		strcpy(version, "1.2.0");
+		strcpy(version, "1.2.1");
 		region_and_motifs = NULL;
 		input_bam = NULL;
 		vntr_bed = NULL;
