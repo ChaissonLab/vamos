9dbbf3717ff4810a5550883b9d5ee6e66535f4c9
Mark Chaisson <mchaisso@gmail.com>
2023-12-15 12:16:26 -0800

    Bumped version to reflect bug fix.

--- src/io.h
+++ src/io.h
@@ -356,7 +356,7 @@ public:
     int phaseFlank;
     IO() 
     {
-      version = "1.3.6";
+      version = "1.3.7";
         region_and_motifs = "";
         input_bam = "";
         vntr_bed = "";
