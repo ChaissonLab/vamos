66e6ae7168052248db6aa429694a903a772e11a9
Mark Chaisson <mchaisso@usc.edu>
2024-07-09 11:34:21 -0700

    Version bump

--- src/io.h
+++ src/io.h
@@ -356,7 +356,7 @@ public:
     int phaseFlank;
     IO() 
     {
-      version = "1.3.8";
+      version = "2.1.2";
         region_and_motifs = "";
         input_bam = "";
         vntr_bed = "";
